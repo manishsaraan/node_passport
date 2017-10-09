b334902852c8f8f302ce37134a6b4bc9a8b871df
manishsaraan <manish021js@gmail.com>
2017-10-09 14:54:20 +0530

    local login implemented.

--- app/routes.js
+++ app/routes.js
@@ -17,9 +17,14 @@ module.exports = function(app, passport) {
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', { message: req.flash('loginMessage') }); 
     });
+   // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-    // process the login form
-    // app.post('/login', do all our passport stuff here);
+   
 
     // =====================================
     // SIGNUP ==============================
@@ -30,7 +35,7 @@ module.exports = function(app, passport) {
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
-    
+
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
