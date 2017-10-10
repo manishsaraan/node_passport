623b8cd4f75555aaa6d9fb59f02c2a30267acf7a
manishsaraan <manish021js@gmail.com>
2017-10-10 10:17:07 +0530

    fb login implemented

--- app/routes.js
+++ app/routes.js
@@ -55,7 +55,18 @@ module.exports = function(app, passport) {
             user : req.user // get the user out of session and pass to template
         });
     });
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
 
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
     // =====================================
     // LOGOUT ==============================
     // =====================================
