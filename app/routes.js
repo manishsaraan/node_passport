170a03bff15f686e19ecf99ba3021b828390ad5c
manishsaraan <manish021js@gmail.com>
2017-10-12 09:51:05 +0530

    changes for google login

--- app/routes.js
+++ app/routes.js
@@ -80,7 +80,20 @@ module.exports = function(app, passport) {
             successRedirect : '/profile',
             failureRedirect : '/'
         }));
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            }));
     // =====================================
     // LOGOUT ==============================
     // =====================================
